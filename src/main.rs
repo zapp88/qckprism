7d9eb97b9b5510024da58ed1bf84e58a303ba01d
Jakub Maciej <zapp88@gmail.com>
2020-10-06 21:18:18 +0200

    Code formatting

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod qck;
 
 fn main() -> rusb::Result<()> {
     let args = cli::fetch_cli_args();
-    qck::send_to_device(qck::Command{
-        light_level : args.light_level,
-        first_color : args.first_color,
-        second_color : args.second_color,
+    qck::send_to_device(qck::Command {
+        light_level: args.light_level,
+        first_color: args.first_color,
+        second_color: args.second_color,
     })
 }
