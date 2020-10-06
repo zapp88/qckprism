a4a3e3791716c55d3a75735bfd7af3730e2a96d3
Jakub Maciej <zapp88@gmail.com>
2020-10-06 21:16:08 +0200

    Code cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ mod cli;
 mod qck;
 
 fn main() -> rusb::Result<()> {
-    let args = cli::fetch_args();
+    let args = cli::fetch_cli_args();
     qck::send_to_device(qck::Command{
         light_level : args.light_level,
-        first_color : args.first_color as qck::Color,
-        second_color : args.second_color as qck::Color,
+        first_color : args.first_color,
+        second_color : args.second_color,
     })
 }
